cf1da5910018b6d7724fe45cc39251e2f171662b
Ragadeepika-Pucha <rpucha@email.arizona.edu>
2023-03-29 10:14:19 -0700

    Updated the function to get the fitting windows

--- py/measure_fits.py
+++ py/measure_fits.py
@@ -5,6 +5,7 @@ The script consists of following functions:
     2) lamspace_to_velspace(del_lam, lam_ref)
     3) velspace_to_lamspace(vel, lam_ref)
     4) compute_noise_emline(lam_rest, flam_rest, model, em_line)
+    5) compute_emline_flux(amplitude, stddev, amplitude_err, stddev_err)
     
 Author : Ragadeepika Pucha
 Version : 2023, March 27
@@ -50,16 +51,27 @@ def calculate_red_chi2(data, model, ivar, n_free_params):
     
 ####################################################################################################
 
-def lamspace_to_velspace(del_lam, lam_ref):
+def lamspace_to_velspace(del_lam, lam_ref, del_lam_err = None, lam_ref_err = None):
     """
     This function converts delta_wavelength from wavelength space to velocity space.
+    Error is computed if the delta_wavelength and reference wavelength errors are given.
     
     Parameters 
     ----------
     del_lam : float
         FWHM or sigma in wavelength units
+        
     lam_ref : float
         Reference wavelength for the conversion
+        
+    del_lam_err : float
+        Error in FWHM of sigma in wavelength units
+        Default is None
+        
+    lam_ref_err : float
+        Error in reference wavelength
+        Default is None
+        
         
     Returns
     -------
@@ -68,10 +80,13 @@ def lamspace_to_velspace(del_lam, lam_ref):
     """
     ## Speed of light in km/s
     c = 2.99792e+5
-    
     vel = (del_lam/lam_ref)*c
     
-    return (vel)
+    if ((del_lam_err is not None)&(lam_ref_err is not None)):
+        vel_err = vel*np.sqrt(((del_lam_err/del_lam)**2)+((lam_ref_err/lam_ref)**2))
+        return (vel, vel_err)
+    else:
+        return (vel)
     
 ####################################################################################################
 
@@ -139,11 +154,46 @@ def compute_noise_emline(lam_rest, flam_rest, model, em_line):
     
     lam_region = lam_rest[lam_ii]
     flam_region = flam_rest[lam_ii]
-    model_region = model(lam_ii)
-    
+    model_region = model(lam_region)
+
     res = flam_region - model_region
     noise = np.std(res)
     
     return (noise)
+
+####################################################################################################
+
+def compute_emline_flux(amplitude, stddev, amplitude_err = None, stddev_err = None):
+    """
+    Function to compute emission-line flux, given it is modeled as a Gaussian.
+    
+    Parameters
+    ----------
+    amplitude : float
+        Amplitude of the Gaussian
+        
+    stddev : float
+        Standard Deviation of the Gaussian
+        
+    amplitude_err : float
+        Amplitude error of the Gaussian. Default is None.
+        
+    stddev_err : float
+        Standard Deviation error of the Gaussian. Default is None.
+        
+    Returns
+    -------
+    flux : float
+        Flux of the emission-line which is modeled as a Gaussian
+    """
+    
+    
+    flux = np.sqrt(2*np.pi)*amplitude*stddev
+    
+    if ((amplitude_err is not None)&(stddev_err is not None)):
+        flux_err = flux*np.sqrt(((amplitude_err/amplitude)**2) + ((stddev_err/stddev)**2))
+        return (flux, flux_err)
+    else:
+        return (flux)
 
 ####################################################################################################
