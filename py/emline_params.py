52b5b265498ad263810c00725546d4dfc1581f8b
Ragadeepika-Pucha <rpucha@email.arizona.edu>
2024-03-10 17:20:04 -0700

    Added a class with two functions for getting the total parameters of the normal and extreme fits

--- py/emline_params.py
+++ py/emline_params.py
@@ -1,9 +1,12 @@
 """
 This script consists of functions for computing the parameters of the emission-line fits.
 It consists of the following functions:
+    1) get_parameters(gfit, models)
+    2) get_allfit_params.normal_fit(fits, lam, flam)
+    3) get_allfit_params.extreme_fit(fits, lam, flam)
 
 Author : Ragadeepika Pucha
-Version : 2024, February 8
+Version : 2024, March 10
 """
 
 ###################################################################################################
@@ -14,7 +17,6 @@ import measure_fits as mfit
 import spec_utils
 from astropy.stats import sigma_clipped_stats
 ###################################################################################################
-
 
 def get_parameters(gfit, models):
     """
@@ -67,206 +69,160 @@ def get_parameters(gfit, models):
     return (params)
     
 ###################################################################################################
+###################################################################################################
 
-# def compute_final_rchi2(lam, flam, ivar, fits, dof):
+class get_allfit_params:
+    """
+    Functions to get all the parameters together.
+        1) normal_fit(fits, lam, flam)
+        2) extreme_fit(fits, lam, flam)
+    """
     
-#     gfit_hb, gfit_oiii, gfit_nii_ha, gfit_sii = fits
-#     hb_dof, oiii_dof, nii_ha_dof, sii_dof = dof
-    
-#     ## Fitting windows for the different emission-lines.
-    
-#     lam_hb, flam_hb, ivar_hb = spec_utils.get_fit_window(lam, flam, \
-#                                                          ivar, em_line = 'hb')
-
-#     lam_oiii, flam_oiii, ivar_oiii = spec_utils.get_fit_window(lam, flam, \
-#                                                                ivar, em_line = 'oiii')
-#     lam_nii_ha, flam_nii_ha, ivar_nii_ha = spec_utils.get_fit_window(lam, flam, \
-#                                                                      ivar, em_line = 'nii_ha')
-#     lam_sii, flam_sii, ivar_sii = spec_utils.get_fit_window(lam, flam, \
-#                                                             ivar, em_line = 'sii') 
-    
-    
+    def normal_fit(fits, lam, flam):
+        """
+        Function to get all the required parameters for the Hb, [OIII], [NII]+Ha, and [SII] fits.
+        This is for the normal source fitting method.
         
-#     hb_rchi2 = mfit.calculate_red_chi2(flam_hb, gfit_hb(lam_hb), ivar_hb, hb_dof)
-#     oiii_rchi2 = mfit.calculate_red_chi2(flam_oiii, gfit_oiii(lam_oiii), ivar_oiii, oiii_dof)
-#     nii_ha_rchi2 = mfit.calculate_red_chi2(flam_nii_ha, gfit_nii_ha(lam_nii_ha), ivar_nii_ha, nii_ha_dof)
-#     sii_rchi2 = mfit.calculate_red_chi2(flam_sii, gfit_sii(lam_sii), ivar_sii, sii_dof)
-    
-#     return (hb_rchi2, oiii_rchi2, nii_ha_rchi2, sii_rchi2)
-
-# ###################################################################################################
-
-
-# def get_bestfit_parameters(table, models, emline):
-#     """
-#     Get the bestfit parameters from iterations of fits
-    
-#     Parameters
-#     ----------
-#     table : Astropy Table
-#         Table of gaussian fits are different iterations
-    
-#     models : list
-#         List of total submodels expected from a given emission-line fitting.
-        
-#     Returns
-#     -------
-#     params : dict
-#         Dictionary with the parameter values
-#     """
-    
-#     params = {}
-    
-#     for model in models:
-#         amplitude_arr = table[f'{model}_amplitude'].data
-#         mean_arr = table[f'{model}_mean'].data
-#         std_arr = table[f'{model}_std'].data
-#         flux_arr = table[f'{model}_flux'].data
-#         sigma_arr = table[f'{model}_sigma'].data
-        
-#         amp_zero = (np.all(np.isclose(amplitude_arr, 0.0)))
-#         mean_zero = (np.all(np.isclose(mean_arr, 0.0)))
-#         std_zero = (np.all(np.isclose(std_arr, 0.0)))
-        
-#         allzero = amp_zero|mean_zero|std_zero
-        
-#         if (allzero):
-#             amp = 0.0
-#             amp_err = 0.0
-#             mean = 0.0
-#             mean_err = 0.0
-#             std = 0.0
-#             std_err = 0.0
-#             flux = 0.0
-#             flux_err = 0.0
-#             sigma = 0.0
-#             sigma_err = 0.0
-#             flux_fits = 0.0
-#             flux_err_fits = 0.0
-#             sigma_fits = 0.0
-#             sigma_err_fits = 0.0
-#         else:
-#             cond = (amplitude_arr > 0)&(mean_arr > 0)&(std_arr > 0)
+        Parameters
+        ----------
+        fits : list
+            List of [Hb, [OIII], [NII]+Ha, [SII]] bestfits
             
-#             amp = np.nanmean(np.where(~cond, np.nan, amplitude_arr))
-#             amp_err = np.nanstd(np.where(~cond, np.nan, amplitude_arr))
-#             mean = np.nanmean(np.where(~cond, np.nan, mean_arr))
-#             mean_err = np.nanstd(np.where(~cond, np.nan, mean_arr))
-#             var = np.where(~cond, np.nan, std_arr)**2
-#             std = np.sqrt(np.nanmean(var))
-#             std_err = np.sqrt(np.nanstd(var))
-#             flux, flux_err = mfit.compute_emline_flux(amp, std, amp_err, std_err)
-#             sigma, sigma_err = mfit.lamspace_to_velspace(std, mean, std_err, mean_err)
-#             flux_fits = np.nanmean(np.where(~cond, np.nan, flux_arr))
-#             flux_err_fits = np.nanstd(np.where(~cond, np.nan, flux_arr))
-#             sigma_fits = np.nanmean(np.where(~cond, np.nan, sigma_arr))
-#             sigma_err_fits = np.nanstd(np.where(~cond, np.nan, sigma_arr))
+        lam : array
+            Wavelength array of the spectra
             
-#         params[f'{model}_amplitude'] = [amp]
-#         params[f'{model}_amplitude_err'] = [amp_err]
-#         params[f'{model}_mean'] = [mean]
-#         params[f'{model}_mean_err'] = [mean_err]
-#         params[f'{model}_std'] = [std]
-#         params[f'{model}_std_err'] = [std_err]
-#         params[f'{model}_sigma'] = [sigma]
-#         params[f'{model}_sigma_err'] = [sigma_err]
-#         params[f'{model}_flux'] = [flux]
-#         params[f'{model}_flux_err'] = [flux_err]
-#         params[f'{model}_flux_fits'] = [flux_fits]
-#         params[f'{model}_flux_err_fits'] = [flux_err_fits]
-#         params[f'{model}_sigma_fits'] = [sigma_fits]
-#         params[f'{model}_sigma_err_fits'] = [sigma_err_fits]
-        
-#         ## Continuum computation 
-#         cont_col = table[f'{emline}_continuum'].data
-#         if (np.all(np.isclose(cont_col, 0.0))):
-#             cont = 0.0
-#             cont_err = 0.0
-#         else:
-#             cont = np.nanmean(np.where(np.isclose(cont_col, 0.0), np.nan, cont_col))
-#             cont_err = np.nanstd(np.where(np.isclose(cont_col, 0.0), np.nan, cont_col))
+        flam : array
+            Flux array of the spectra
             
-#         params[f'{emline}_continuum'] = [cont]
-#         params[f'{emline}_continuum_err'] = [cont_err]
-        
-#     return (params)
-
-# ####################################################################################################
-
-# def fix_params(table):
-#     emlines = ['hb', 'nii6548', 'nii6583', 'ha']
-    
-#     for emline in emlines:
-#         if ((emline == 'hb')|(emline == 'ha')):
-#             amp_n = table[f'{emline}_n_amplitude'].data[0]
-#             mean_n = table[f'{emline}_n_mean'].data[0]
-#             std_n = table[f'{emline}_n_std'].data[0]
-#             flux_n = table[f'{emline}_n_flux'].data[0]
-#             sig_n = table[f'{emline}_n_sigma'].data[0]
-#         else:
-#             amp_n = table[f'{emline}_amplitude'].data[0]
-#             mean_n = table[f'{emline}_mean'].data[0]
-#             std_n = table[f'{emline}_std'].data[0]
-#             flux_n = table[f'{emline}_flux'].data[0]
-#             sig_n = table[f'{emline}_sigma'].data[0]
-        
-#         amp_out = table[f'{emline}_out_amplitude'].data
-#         mean_out = table[f'{emline}_out_mean'].data[0]
-#         std_out = table[f'{emline}_out_std'].data[0]
-#         flux_out = table[f'{emline}_out_flux'].data[0]
-#         sig_out = table[f'{emline}_out_sigma'].data[0]
-        
-#         if ((amp_n == 0)&(amp_out != 0)):
-#             if ((emline == 'hb')|(emline == 'ha')):
-#                 table[f'{emline}_n_amplitude'][0] = amp_out
-#                 table[f'{emline}_n_mean'][0] = mean_out
-#                 table[f'{emline}_n_std'][0] = std_out
-#                 table[f'{emline}_n_flux'][0] = flux_out
-#                 table[f'{emline}_n_sigma'][0] = sig_out
-#             else:
-#                 table[f'{emline}_amplitude'][0] = amp_out
-#                 table[f'{emline}_mean'][0] = mean_out
-#                 table[f'{emline}_std'][0] = std_out
-#                 table[f'{emline}_flux'][0] = flux_out
-#                 table[f'{emline}_sigma'][0] = sig_out
+        Returns
+        -------
+        hb_params : dict
+            Gaussian parameters of the Hb fit, 
+            followed by continuum and noise measurements.
             
-#             table[f'{emline}_out_amplitude'][0] = amp_n
-#             table[f'{emline}_out_mean'][0] = mean_n
-#             table[f'{emline}_out_std'][0] = std_n
-#             table[f'{emline}_out_flux'][0] = flux_n
-#             table[f'{emline}_out_sigma'][0] = sig_n
+        oiii_params : dict
+            Gaussian parameters of the [OIII] fit, 
+            followed by continuum and noise measurements.
             
-    
-#     return (table)
+        nii_ha_params : dict
+            Gaussian parameters of the [NII]+Ha fit, 
+            followed by continuum and noise measurements.
+            
+        sii_params : dict
+            Gaussian parameters of the [SII] fit,
+            followed by continuum and noise measurements.
+        """
+        
+        
+        gfit_hb, gfit_oiii, gfit_nii_ha, gfit_sii = fits
 
-# ####################################################################################################
+        ## Parameters for the fit
+        hb_models = ['hb_n', 'hb_out', 'hb_b']
+        oiii_models = ['oiii4959', 'oiii4959_out', 'oiii5007', 'oiii5007_out']
+        nii_ha_models = ['nii6548', 'nii6548_out', 'nii6583', 'nii6583_out', \
+                         'ha_n', 'ha_out', 'ha_b']
+        sii_models = ['sii6716', 'sii6716_out', 'sii6731', 'sii6731_out']
 
-# def calculate_emline_noise(specprod, survey, program, healpix, targetid, z):
+        hb_params = get_parameters(gfit_hb, hb_models)
+        oiii_params = get_parameters(gfit_oiii, oiii_models)
+        nii_ha_params = get_parameters(gfit_nii_ha, nii_ha_models)
+        sii_params = get_parameters(gfit_sii, sii_models)
+
+        ## Continuum
+        hb_params['hb_continuum'] = [gfit_hb['hb_cont'].amplitude.value]
+        oiii_params['oiii_continuum'] = [gfit_oiii['oiii_cont'].amplitude.value]
+        nii_ha_params['nii_ha_continuum'] = [gfit_nii_ha['nii_ha_cont'].amplitude.value]
+        sii_params['sii_continuum'] = [gfit_sii['sii_cont'].amplitude.value]
+
+        ## NOISE
+        hb_noise = mfit.compute_noise_emline(lam, flam, 'hb')
+        oiii_noise = mfit.compute_noise_emline(lam, flam, 'oiii')
+        nii_ha_noise = mfit.compute_noise_emline(lam, flam, 'nii_ha')
+        sii_noise = mfit.compute_noise_emline(lam, flam, 'sii')
+
+        hb_params['hb_noise'] = [hb_noise]
+        oiii_params['oiii_noise'] = [oiii_noise]
+        nii_ha_params['nii_ha_noise'] = [nii_ha_noise]
+        sii_params['sii_noise'] = [sii_noise]
+
+        return (hb_params, oiii_params, nii_ha_params, sii_params)
     
-#     lam_rest, flam_rest, ivar_rest, _ = spec_utils.get_emline_spectra(specprod, survey, program, \
-#                                                                    healpix, targetid, z, \
-#                                                                    rest_frame = True, \
-#                                                                    plot_continuum = False)
+###################################################################################################
+
+    def extreme_fit(fits, lam, flam):
+        """
+        Function to get all the required parameters for the Hb, [OIII], [NII]+Ha, and [SII] fits.
+        This is for the extreme broad-line source fitting.
+        
+        Parameters
+        ----------
+        fits : list
+            List of [Hb+[OIII], [NII]+Ha+[SII]] bestfits
+            
+        lam : array
+            Wavelength array of the spectra
+            
+        flam : array
+            Flux array of the spectra
+            
+        Returns
+        -------
+        hb_params : dict
+            Gaussian parameters of the Hb fit, 
+            followed by continuum and noise measurements.
+            
+        oiii_params : dict
+            Gaussian parameters of the [OIII] fit, 
+            followed by continuum and noise measurements.
+            
+        nii_ha_params : dict
+            Gaussian parameters of the [NII]+Ha fit, 
+            followed by continuum and noise measurements.
+            
+        sii_params : dict
+            Gaussian parameters of the [SII] fit,
+            followed by continuum and noise measurements.
+        """
+        
+        gfit_hb_oiii, gfit_nii_ha_sii = fits
+
+        ## Parameters for the fit
+        hb_models = ['hb_n', 'hb_out', 'hb_b']
+        oiii_models = ['oiii4959', 'oiii4959_out', 'oiii5007', 'oiii5007_out']
+        nii_ha_models = ['nii6548', 'nii6548_out', 'nii6583', 'nii6583_out', \
+                         'ha_n', 'ha_out', 'ha_b']
+        sii_models = ['sii6716', 'sii6716_out', 'sii6731', 'sii6731_out']
+
+        hb_params = get_parameters(gfit_hb_oiii, hb_models)
+        oiii_params = get_parameters(gfit_hb_oiii, oiii_models)
+        nii_ha_params = get_parameters(gfit_nii_ha_sii, nii_ha_models)
+        sii_params = get_parameters(gfit_nii_ha_sii, sii_models)
+
+        ## Continuum
+        hb_params['hb_continuum'] = [gfit_hb_oiii['hb_oiii_cont'].amplitude.value]
+        oiii_params['oiii_continuum'] = [gfit_hb_oiii['hb_oiii_cont'].amplitude.value]
+        nii_ha_params['nii_ha_continuum'] = [gfit_nii_ha_sii['nii_ha_sii_cont'].amplitude.value]
+        sii_params['sii_continuum'] = [gfit_nii_ha_sii['nii_ha_sii_cont'].amplitude.value]
+
+        ## NOISE
+        hb_noise = mfit.compute_noise_emline(lam, flam, 'hb')
+        oiii_noise = mfit.compute_noise_emline(lam, flam, 'oiii')
+        nii_ha_noise = mfit.compute_noise_emline(lam, flam, 'nii_ha')
+        sii_noise = mfit.compute_noise_emline(lam, flam, 'sii')
+
+        hb_params['hb_noise'] = [hb_noise]
+        oiii_params['oiii_noise'] = [oiii_noise]
+        nii_ha_params['nii_ha_noise'] = [nii_ha_noise]
+        sii_params['sii_noise'] = [sii_noise]
+
+        return (hb_params, oiii_params, nii_ha_params, sii_params)
+
+###################################################################################################
+###################################################################################################
     
-#     hb_noise = mfit.compute_noise_emline(lam_rest, flam_rest, em_line = 'hb')
-#     oiii_noise = mfit.compute_noise_emline(lam_rest, flam_rest, em_line = 'oiii')
-#     nii_ha_noise = mfit.compute_noise_emline(lam_rest, flam_rest, em_line = 'nii_ha')
-#     sii_noise = mfit.compute_noise_emline(lam_rest, flam_rest, em_line = 'sii')
     
-#     params = {}
-#     params['TARGETID'] = [targetid]
-#     params['HB_NOISE'] = [hb_noise]
-#     params['OIII_NOISE'] = [oiii_noise]
-#     params['NII_HA_NOISE'] = [nii_ha_noise]
-#     params['SII_NOISE'] = [sii_noise]
     
-#     t_params = Table(params)
-    
-#     #t_params.write(f'output/single_files/emfit-noise-{targetid}.fits')
-    
-#     return (t_params)
-    
-# ####################################################################################################
+
 
         
     
