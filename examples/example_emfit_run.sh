eab87af15a96e2623f50d53b7ed16dc7badbde3f
Ragadeepika-Pucha <rpucha@email.arizona.edu>
2024-04-18 18:20:53 -0700

    Added comments in the sbatch related files

--- examples/example_emfit_run.sh
+++ examples/example_emfit_run.sh
@@ -2,8 +2,10 @@
 
 cd
 source .bashrc
-cd $PATH_TO_WORKING_DIR
+## Change $PATH_TO_WORKING_DIR --> Path to the directory where the EmFit is being run
+cd $PATH_TO_WORKING_DIR          
 
 source /global/cfs/cdirs/desi/software/desi_environment.sh 22.5
 
-python $PATH_TO_EMFIT_DIR/py/run_desi_emfit.py INPUT_FILE_NAME OUTPUT_FILE_NAME
+## Change $PATH_TO_EMFIT_DIR --> Path to the EmFit repository in your system
+python $PATH_TO_EMFIT_DIR/py/run_desi_emfit.py INPUT_FILE_NAME OUTPUT_FILE_NAME       
