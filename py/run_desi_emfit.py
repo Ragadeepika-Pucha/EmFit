2e9b2d89d5a19adf5fe6c7dc23709394b3d263b2
Ragadeepika-Pucha <rpucha@email.arizona.edu>
2025-03-02 13:28:41 -0800

    Modified the run_desi_emfit script to work with survey/program/healpix divisions

--- py/run_desi_emfit.py
+++ py/run_desi_emfit.py
@@ -3,7 +3,7 @@ This script is for running the DESI EmFit Code for a given table of sources.
 It requires input and output filenames
 
 Author : Ragadeepika Pucha
-Version : 2024 March 28
+Version : 2025 March 02
 """
 ####################################################################################################
 import sys
@@ -12,6 +12,7 @@ sys.path.append('/global/cfs/cdirs/desi/users/raga19/repos/DESI_Project/py/')
 
 import numpy as np
 import emline_fitting as emfit
+import spec_utils
 from astropy.table import Table, vstack
 
 import warnings
@@ -30,12 +31,108 @@ start = time.time()
 
 t = Table.read(filename)
 
-pool = Pool(processes = 128)
-inputs = [(obj['SPECPROD'], obj['SURVEY'], obj['PROGRAM'], obj['HEALPIX'],\
-           obj['TARGETID'], obj['Z']) for obj in t]
-t_final = vstack(pool.starmap(emfit.fit_spectra, inputs))
-pool.close()
-pool.join()
+t_outs = []
+
+## Empty arrays for collecting data related to galaxies
+t_ins = []
+t_fmeta = []
+models_arr = np.ndarray((0,3,7781))
+lam_arr = []
+flam_arr = []
+ivar_arr = []
+ebv_arr = []
+tgt_arr = []
+rsigma_arr = []
+res_arr = []
+
+## Group by SPECPROD-SURVEY-PROGRAM-HEALPIX
+specprods = np.unique(t['SPECPROD'].astype(str))
+
+## Group by specprod
+for specprod in specprods:
+    t_spec = t[t['SPECPROD'].astype(str) == specprod]
+
+    ## Group by surveys
+    surveys = np.unique(t_spec['SURVEY'].astype(str))
+    for survey in surveys:
+        t_surv = t_spec[t_spec['SURVEY'].astype(str) == survey]
+
+        ## Group by programs
+        programs = np.unique(t_surv['PROGRAM'].astype(str))
+        for program in programs:
+            t_prog = t_surv[t_surv['PROGRAM'].astype(str) == program]
+
+            ## Group by healpix
+            healpix = np.unique(t_prog['HEALPIX'])
+            for hpx in healpix:
+                t_hpx = t_prog[t_prog['HEALPIX'] == hpx]
+
+                print (f'------------------------------ {survey} {program} {hpx} ----------------------------- {len(t_hpx)} galaxies -------------------------------')
+
+                ## List of all targets
+                targets = t_hpx['TARGETID'].data
+
+                ## Coadded-Spectra, FastSpecFit files
+                coadd_spec, fmeta, models = spec_utils.get_spectra_fastspec_data(specprod, survey,\
+                                                                                 program, hpx,\
+                                                                                 targets)
+
+                ## Required parameters
+                lam = coadd_spec.wave['brz']
+                flams = coadd_spec.flux['brz']
+                ivars = coadd_spec.ivar['brz']
+                ebvs = coadd_spec.fibermap['EBV'].data
+                tgt_ids = coadd_spec.target_ids().data
+                
+                rsigmas = spec_utils.compute_resolution_sigma(coadd_spec)
+                res_matrices = coadd_spec.R['brz']
+
+                ## Collecting the data
+                t_ins.append(t_hpx)
+                t_fmeta.append(fmeta)
+                models_arr = np.concatenate([models_arr, models])
+                flam_arr.extend(flams)
+                ivar_arr.extend(ivars)
+                ebv_arr.extend(ebvs)
+                tgt_arr.extend(tgt_ids)
+                rsigma_arr.extend(rsigmas)
+                res_arr.extend(res_matrices)
+
+                if ((len(flam_arr) >= 250)|((specprod == specprods[-1])&(survey == surveys[-1])&(program == programs[-1])&(hpx == healpix[-1]))):
+                    ## If the number of sources crosses 250 then run the code
+                    run_start = time.time()
+                    t_in = vstack(t_ins)
+                    fmeta = vstack(t_fmeta)
+                    print (f'==================================== Fitting {len(t_in)} galaxies ==============================================================')
+
+                    ## Multiprocessing 
+                    args = [(t_in, fmeta, models_arr, tgt_arr[kk], lam, flam_arr[kk], \
+                             ivar_arr[kk], ebv_arr[kk], rsigma_arr[kk], res_arr[kk]) for kk in range(len(t_in))]
+                    pool = Pool(processes = 256)
+                    t_fit = vstack(pool.starmap(emfit.fit_single_spectrum, args))
+                    pool.close()
+                    pool.join()
+            
+                    t_outs.append(t_fit)
+            
+                    ## Reset the arrays
+                    t_ins = []
+                    t_fmeta = []
+                    models_arr = np.ndarray((0,3,7781))
+                    lam_arr = []
+                    flam_arr = []
+                    ivar_arr = []
+                    ebv_arr = []
+                    tgt_arr = []
+                    rsigma_arr = []
+                    res_arr = []
+
+                    run_end = time.time()
+                    print (f'==================================== Fitting Time: {round(run_end - run_start, 2)} sec =========================================')
+
+## Join all tables
+t_final = vstack(t_outs)
+print (f'================================== Total ================================== {len(t_final)} galaxies ================================================')        
 
 t_final.write(outfile, overwrite = True)
 
